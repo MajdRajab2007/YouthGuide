0cdca3e23b5ed209401f15af2f191a0b6a6fcf5a
Majd Rajab <austmajd@gmail.com>
2024-01-14 23:50:13 +0300

    Admin Dashboard

--- app/Models/Posts.php
+++ app/Models/Posts.php
@@ -2,10 +2,15 @@
 
 namespace App\Models;
 
+use Backpack\CRUD\app\Models\Traits\CrudTrait;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Posts extends Model
 {
+    use CrudTrait;
     use HasFactory;
+    
 }
--- database/migrations/2024_01_14_203504_create_tags_table.php
+++ database/migrations/2024_01_14_203504_create_tags_table.php
@@ -11,16 +11,9 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('posts', function (Blueprint $table) {
+        Schema::create('tags', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
-            $table->string('title');
-            $table->text('body');
-            $table->string('label');
-            $table->string('image')->nullable();
-            $table->foreignId('user_id')->constrained();
-
-
         });
     }
 
@@ -29,6 +22,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('posts');
+        Schema::dropIfExists('tags');
     }
 };
