216874df64a021249351c693a9899f9bce9ce616
adham11mahfoud19 <adham111mahfoud@gmail.com>
2024-01-21 11:10:57 +0000

    adham

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -8,7 +8,7 @@ use Illuminate\Http\Request;
 class PostsController extends Controller
 {
     public function showByTag(Request $request){
-        
+
         $posts = Posts::where('tag_name',$request->tag_name)->get();
 
 
@@ -26,19 +26,19 @@ class PostsController extends Controller
 
                 'status' => false,
                 'message' => 'Post not found.',
-            
+
             ]);
     }
 }
 
 public function showByTagAndLanguage(Request $request){
-        
+
     $posts = Posts::
     where('tag_name',$request->tag_name)
     ->where('coding_language_name',$request->coding_language_name)
     ->get();
-    
-    
+
+
 
 
     if ($posts) {
@@ -55,11 +55,39 @@ public function showByTagAndLanguage(Request $request){
 
             'status' => false,
             'message' => 'Post not found.',
-        
+
         ]);
 }
 }
+public function showByTagAndLanguageAndId(Request $request){
 
+    $posts = Posts::
+    where('tag_name',$request->tag_name)
+    ->where('coding_language_name',$request->coding_language_name)
+    ->where('id',$request->id)
+    ->get();
+
+
+
+
+    if ($posts) {
+
+        return response()->json([
+            'status' => true,
+            'message' => 'Post found successfully.',
+            'data' => $posts
+        ]);
+
+    } else {
+
+        return response()->json([
+
+            'status' => false,
+            'message' => 'Post not found.',
+
+        ]);
+}
+}
 public function showById(Request $request){
     $posts = Posts::where('id',$request->id)->first();
 
