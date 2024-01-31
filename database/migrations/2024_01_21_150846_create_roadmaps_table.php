373cf744f2d0e2268fe633667fe17bb10d57fdac
Majd Rajab <austmajd@gmail.com>
2024-01-31 11:32:05 +0300

    IDK

--- database/migrations/2024_01_21_150846_create_roadmaps_table.php
+++ database/migrations/2024_01_21_150846_create_roadmaps_table.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
-use App\Models\CodingLanguage;
+
 return new class extends Migration
 {
     /**
@@ -11,20 +11,13 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('posts', function (Blueprint $table) {
-            
+        Schema::create('roadmaps', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
             $table->string('title');
             $table->text('body');
             $table->text('review');
             $table->string('image')->nullable();
-            $table->foreignId('tag_name');
-            $table->foreignId('team_name');
-            $table->foreignId('coding_language_name')->nullable();
-            $table->foreignId('design_type_name')->nullable();
-
-
         });
     }
 
@@ -33,6 +26,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('posts');
+        Schema::dropIfExists('roadmaps');
     }
 };
