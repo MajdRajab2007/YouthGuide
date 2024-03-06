deb195ae06e20cbdf281be2dfb0e67937e67ac07
Majd Rajab <austmajd@gmail.com>
2024-03-06 16:42:04 +0300

    Adding build to server

---
+++ resources/views/index.blade.php
@@ -0,0 +1,15 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <link rel="icon" type="image/svg+xml" href="/assets/logo-WFx706gD.png" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>دليل الشباب</title>
+    <script type="module" crossorigin src="/assets/index-cFrXWdcz.js"></script>
+    <link rel="stylesheet" crossorigin href="/assets/index-GCs2zMl3.css">
+  </head>
+  <body>
+	<script src="js/app.js"></script>
+    <div id="root"></div>
+  </body>
+</html>
